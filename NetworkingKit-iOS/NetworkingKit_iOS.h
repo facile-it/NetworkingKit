2622ed0da498026c6c76272bf26dc557822046b8
Christian Miranti <christian.miranti@facile.it>
2018-01-19 14:47:21 +0100

    FPI-26 add iOS target

---
+++ NetworkingKit-iOS/NetworkingKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingKit_iOS.h
+//  NetworkingKit-iOS
+//
+//  Created by Christian Miranti on 19/01/18.
+//  Copyright © 2018 Facile.it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkingKit_iOS.
+FOUNDATION_EXPORT double NetworkingKit_iOSVersionNumber;
+
+//! Project version string for NetworkingKit_iOS.
+FOUNDATION_EXPORT const unsigned char NetworkingKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingKit_iOS/PublicHeader.h>
+
+
