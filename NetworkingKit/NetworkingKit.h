330746ed5638a3cab6d9a3534a7961143620cbf5
Christian Miranti <christian.miranti@facile.it>
2018-01-19 10:37:24 +0100

    Start

---
+++ NetworkingKit/NetworkingKit.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingKit.h
+//  NetworkingKit
+//
+//  Created by Christian Miranti on 19/01/18.
+//  Copyright © 2018 Facile.it. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NetworkingKit.
+FOUNDATION_EXPORT double NetworkingKitVersionNumber;
+
+//! Project version string for NetworkingKit.
+FOUNDATION_EXPORT const unsigned char NetworkingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingKit/PublicHeader.h>
+
+
