f9288ffacd65cd09644bd210e6591957ce65fe01
Christian Miranti <christian.miranti@facile.it>
2018-01-29 16:43:53 +0100

    FPI-26 update JSONString type

--- NetworkingKit/Client/Utilites.swift
+++ NetworkingKit/Client/Utilites.swift
@@ -1,18 +1,38 @@
 import Foundation
+import FunctionalKit
 
 struct JSONString {
-	static func from(_ object: Any) -> String {
+	static func from(_ object: Any) -> JSONStringResult {
 		guard JSONSerialization.isValidJSONObject(object) else {
-			return "Invalid JSON object"
+			return .failure(.invalidJSONObject)
 		}
 		guard let data = try? JSONSerialization.data(withJSONObject: object, options: JSONSerialization.WritingOptions(rawValue: 0)) else {
-			return "Error in creating data from valid JSON object"
+			return .failure(.dataCreationFailed)
 		}
 		guard let decoded = String(data: data, encoding: String.Encoding.utf8) else {
-			return "Cannot create string from valid JSON data"
+			return .failure(.stringCreationFailed)
 		}
-		return decoded
+		return .success(decoded
 			.replacingOccurrences(of: "\n", with: "")
-			.replacingOccurrences(of: "\\/", with: "/")
+			.replacingOccurrences(of: "\\/", with: "/"))
 	}
 }
+
+enum JSONStringError: Error, CustomStringConvertible {
+    case invalidJSONObject
+    case dataCreationFailed
+    case stringCreationFailed
+    
+    var description: String {
+        switch self {
+        case .invalidJSONObject:
+            return "Invalid JSON object"
+        case .dataCreationFailed:
+            return "Error in creating data from valid JSON object"
+        case .stringCreationFailed:
+            return "Cannot create string from valid JSON data"
+        }
+    }
+}
+
+typealias JSONStringResult = Result<JSONStringError,String>
