aceb37b01d45e666259dd2ee786d2b6913337b5c
Christian Miranti <christian.miranti@facile.it>
2018-01-29 15:06:14 +0100

    FPI-26 add more tests

--- NetworkingKitTests/Client/ConnectionInfoTests.swift
+++ NetworkingKitTests/Client/ConnectionInfoTests.swift
@@ -14,4 +14,20 @@ class ConnectionInfoTests: XCTestCase {
 			Law<ConnectionInfo>.isAssociative(a, b, c)
 		}
 	}
+    
+    func testConnectionInfoJSONObject() {
+        property("Serialize a ConnectionInfo with `toJSON` always return a ClientResult.success") <- forAll { (ci: ConnectionInfo) in
+            Serialize.toJSON(ci)
+                .fold(
+                    onSuccess: { _ in true },
+                    onFailure: { _ in false })
+        }
+        
+        property("Serialize a ConnectionInfo with `toJSON` always return a ClientResult.success") <- forAll { (ci: ConnectionInfo) in
+            Serialize.fromJSONObject(ci.toJSONObject)
+                .fold(
+                    onSuccess: { _ in true },
+                    onFailure: { _ in false })
+        }
+    }
 }
