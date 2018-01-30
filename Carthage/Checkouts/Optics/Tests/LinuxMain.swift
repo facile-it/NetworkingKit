3687a873f6f71b79bf667145ad424b27318684f1
Christian Miranti <christian.miranti@facile.it>
2018-01-30 09:43:46 +0100

    FPI-26 delete and ignore Carthage/Checkouts

--- Carthage/Checkouts/Optics/Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import OpticsTests
-
-XCTMain([
-	testCase(AdapterTests.allTests),
-	testCase(AffineTests.allTests),
-    testCase(LensTests.allTests),
-    testCase(PrismTests.allTests)
-])
