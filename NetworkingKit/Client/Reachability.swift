a3d2f600250998c6e2fea18c08626618487ae815
Andrea <andrea.rinaldi91@gmail.com>
2021-02-01 15:29:25 +0100

    Move Network class in Reachability and make it public

--- NetworkingKit/Client/Reachability.swift
+++ NetworkingKit/Client/Reachability.swift
@@ -2,12 +2,12 @@ import Foundation
 import SystemConfiguration
 
 public class Reachability {
-
+    
     private var isRunning = false
     private var isReachableOnWWAN: Bool
     private var reachability: SCNetworkReachability?
     private var reachabilityFlags = SCNetworkReachabilityFlags()
-
+    
     public init() throws {
         var zeroAddress = sockaddr_in()
         zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
@@ -23,33 +23,33 @@ public class Reachability {
         isReachableOnWWAN = true
         try start()
     }
-
+    
     deinit { stop() }
-
+    
     public var status: Network.Status {
         return  !isConnectedToNetwork ? .unreachable :
-                isReachableViaWiFi    ? .wifi :
-                isRunningOnDevice     ? .wwan : .unreachable
+            isReachableViaWiFi    ? .wifi :
+            isRunningOnDevice     ? .wwan : .unreachable
     }
-
+    
     public var isRunningOnDevice: Bool = {
         #if targetEnvironment(simulator)
-            return false
+        return false
         #else
-            return true
+        return true
         #endif
     }()
-
+    
     public var isConnectedToNetwork: Bool {
         return isReachable &&
-               !isConnectionRequiredAndTransientConnection &&
-               !(isRunningOnDevice && isWWAN && !isReachableOnWWAN)
+            !isConnectionRequiredAndTransientConnection &&
+            !(isRunningOnDevice && isWWAN && !isReachableOnWWAN)
     }
-
+    
 }
 
 private extension Reachability {
-
+    
     func start() throws {
         guard let reachability = reachability, !isRunning else { return }
         var context = SCNetworkReachabilityContext(version: 0, info: nil, retain: nil, release: nil, copyDescription: nil)
@@ -59,7 +59,7 @@ private extension Reachability {
         }
         isRunning = true
     }
-
+    
     func stop() {
         defer { isRunning = false }
         guard let reachability = reachability else { return }
@@ -67,32 +67,32 @@ private extension Reachability {
         SCNetworkReachabilitySetDispatchQueue(reachability, nil)
         self.reachability = nil
     }
-
+    
     /// Flags that indicate the reachability of a network node name or address, including whether a connection is required, and whether some user intervention might be required when establishing a connection.
     var flags: SCNetworkReachabilityFlags? {
         guard let reachability = reachability else { return nil }
         var flags = SCNetworkReachabilityFlags()
         return withUnsafeMutablePointer(to: &flags) {
             SCNetworkReachabilityGetFlags(reachability, UnsafeMutablePointer($0))
-            } ? flags : nil
+        } ? flags : nil
     }
-
+    
     /// compares the current flags with the previous flags and if changed posts a flagsChanged notification
     func flagsChanged() {
         guard let flags = flags, flags != reachabilityFlags else { return }
         reachabilityFlags = flags
     }
-
+    
     var isReachableViaWiFi: Bool {
         return isReachable && isRunningOnDevice && !isWWAN
     }
-
+    
     /// The specified node name or address can be reached using the current network configuration.
     var isReachable: Bool { return flags?.contains(.reachable) == true }
-
+    
     /// The specified node name or address can be reached via a cellular connection, such as EDGE or GPRS.
     var isWWAN: Bool { return flags?.contains(.isWWAN) == true }
-
+    
     /// The specified node name or address can be reached using the current network configuration, but a connection must first be established. If this flag is set
     /// The specified node name or address can be reached via a transient connection, such as PPP.
     var isConnectionRequiredAndTransientConnection: Bool {
@@ -110,13 +110,18 @@ private func callout(reachability: SCNetworkReachability, flags: SCNetworkReacha
     }
 }
 
-public struct Network {
-
-    enum Status: String {
-        case unreachable, wifi, wwan
+extension Reachability {
+    
+    public struct Network {
+        
+        public enum Status: String {
+            case unreachable, wifi, wwan
+        }
+        
+        public enum Error: Swift.Error {
+            case failedToSetCallout
+            case failedToInitializeWith(sockaddr_in)
+        }
     }
-    enum Error: Swift.Error {
-        case failedToSetCallout
-        case failedToInitializeWith(sockaddr_in)
-    }
+    
 }
